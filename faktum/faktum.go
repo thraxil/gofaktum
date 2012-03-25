9031d791ac4ae44ffcba828776901bcbd0c9935b
Anders Pearson <anders@columbia.edu>
2012-03-25 16:09:31 -0400

    initial commit

---
+++ faktum/faktum.go
@@ -0,0 +1,72 @@
+package facktum
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"http"
+	"template"
+	"time"
+)
+
+// model data structures
+type Fact struct {
+	Title      string
+	Details    string
+	SourceUrl  string
+	SourceName string
+	AddDate    datastore.Time
+	User       string
+}
+
+type Tag struct {
+	Name string
+}
+
+type FactTag struct {
+	Fact string
+	Tag string
+}
+
+// routing
+func init() {
+	http.HandleFunc("/",index)
+	http.HandleFunc("/add/",add)
+}
+
+// controller functions
+
+var indexTmpl = template.Must(template.New("index").ParseFile("templates/index.html"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Fact").Order("-AddDate").Limit(10)
+	facts := make([]Fact, 0, 10)
+	if _, err := q.GetAll(c, &facts); err != nil {
+		http.Error(w, err.String(), http.StatusInternalServerError)
+		return
+	}
+	if err := indexTmpl.Execute(w, facts); err != nil {
+		http.Error(w, err.String(), http.StatusInternalServerError)
+	}
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	f := Fact{
+        Title: r.FormValue("title"),
+	Details: r.FormValue("details"),
+	SourceUrl: r.FormValue("source_url"),
+	SourceName: r.FormValue("source_name"),
+        AddDate:    datastore.SecondsToTime(time.Seconds()),
+	}
+	if u := user.Current(c); u != nil {
+		f.User = u.String()
+	}
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Fact", nil), &f)
+	if err != nil {
+		http.Error(w, err.String(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
